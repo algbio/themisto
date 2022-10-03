8662b42ba5eb06e8ecd00c3733ffb8eac48a548d
Jarno N. Alanko <alanko.jarno@gmail.com>
2022-10-03 16:22:10 +0300

    Delete old KMC submodule

--- KMC/tests/py_kmc_api/run.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import subprocess, sys, os
-#print(os.path.dirname(os.path.abspath(__file__)))
-#sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-subprocess.call(['python3', '-m', 'pytest'])
-#subprocess.call(['pytest'])
