9b6ce2ea22accca08fdab137a39755950c300dc0
Jarno N. Alanko <alanko.jarno@gmail.com>
2022-10-26 00:06:33 +0300

    Concept requirements for the view class

--- include/Color_Set_Interface.hh
+++ include/Color_Set_Interface.hh
@@ -2,16 +2,24 @@
 #include <vector>
 
 template <typename T>
-concept Color_Set_Interface = requires(T& t){
+concept Color_Set_Interface = requires(T& t, T::view_t& view){
     { t.empty() } -> std::same_as<bool>; // This should take constant time
     { t.size() } -> std::same_as<int64_t>; // This may take linear time
     { t.size_in_bits() } -> std::same_as<int64_t>;
     { t.contains(int64_t()) } -> std::same_as<bool>; // This may take linear time
     { t.intersection(t) };
     { t.do_union(t) };
-    //{ t.serialize(os) } -> std::same_as<int64_t>; // Serialization not required for individual color sets any more
-    //{ t.load(is) } -> std::same_as<void>;  // Loading not required for individual color sets any more
     { t.get_colors_as_vector() } -> std::same_as<std::vector<int64_t>>;
     requires std::constructible_from<T, std::vector<int64_t>>;
     requires std::default_initializable<T>;
+
+    // Below are the requirements for the associated view class T::view_t
+    { view.empty() } -> std::same_as<bool>; // This should take constant time
+    { view.size() } -> std::same_as<int64_t>; // This may take linear time
+    { view.size_in_bits() } -> std::same_as<int64_t>;
+    { view.contains(int64_t()) } -> std::same_as<bool>; // This may take linear time
+    { view.get_colors_as_vector() } -> std::same_as<std::vector<int64_t>>;
+    requires std::constructible_from<typename T::view_t, T>;
+    requires std::constructible_from<T, typename T::view_t>;
+    // No intersection and union required for a view because a view is supposed to be immutable.
 };
