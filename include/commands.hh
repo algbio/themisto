4d4d7185171576ce2c28169d99fca93a915c204c
Jarno N. Alanko <alanko.jarno@gmail.com>
2023-07-11 14:59:54 +0100

    Fix

--- include/commands.hh
+++ include/commands.hh
@@ -8,7 +8,7 @@ int dump_color_matrix_main(int argc, char** argv);
 
 int color_set_diagnostics_main(int argc, char** argv); // Undocumented developer feature
 int make_d_equal_1_main(int argc, char** argv); // Undocumented developer feature
-int dump_distinct_color_sets_to_binary(int argc, char** argv); // Undocumented developer feature
+int dump_distinct_color_sets_to_binary_main(int argc, char** argv); // Undocumented developer feature
 
 
 /*
