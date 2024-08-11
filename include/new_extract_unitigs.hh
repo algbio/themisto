30619459769c19aadf0be07b72e8e8d1362f1883
Jarno N. Alanko <alanko.jarno@gmail.com>
2024-08-11 10:43:21 +0300

    Implement colored unitigs (untested)

--- include/new_extract_unitigs.hh
+++ include/new_extract_unitigs.hh
@@ -38,20 +38,45 @@ pair<vector<DBG::Node>, vector<char>> walk_unitig_from(const DBG& dbg, DBG::Node
     return {nodes, label};
 }
 
-void process_unitig_from(const DBG& dbg, DBG::Node v, vector<bool>& visited, ostream& unitigs_out, int64_t unitig_id) {
+template<typename coloring_t>
+void process_unitig_from(const DBG& dbg, const coloring_t& coloring, DBG::Node v, vector<bool>& visited, ostream& unitigs_out, int64_t unitig_id, bool split_by_colorset_runs) {
     vector<DBG::Node> nodes;
+    vector<int64_t> subunitig_ends; // Unitigs broken by color set runs. Exclusive endpoints
     vector<char> label;
     std::tie(nodes, label) = walk_unitig_from(dbg, v);
+    check_true(nodes.size() > 0, "BUG: empty unitig");
 
-    for(DBG::Node u : nodes){
+    int64_t color_set_id = coloring.get_color_set_id(nodes.back());
+
+    for(int64_t pos = (int64_t)nodes.size()-1; pos >= 0; pos--){
+        const DBG::Node& u = nodes[pos];
+
+        if(split_by_colorset_runs){
+            // Color sets can change only at core k-mers. Otherwise the color set is the same as that
+            // of the successor in the DBG.
+            int64_t new_color_set_id = coloring.is_core_kmer(u) ? coloring.get_color_set_id(u) : color_set_id;
+            if(pos == (int64_t)nodes.size()-1 || new_color_set_id != color_set_id) {
+                subunitig_ends.push_back(pos+1); // Start a new subunitig
+            }
+            color_set_id = new_color_set_id;
+        }
+
         assert(!visited[u.id]);
         visited[u.id] = true;
     }
 
-    label.push_back(0); // Make it a C string
-    unitigs_out << ">" << unitig_id << "\n" << label.data() << "\n";
-
-    // TODO: break by color sets
+    if(split_by_colorset_runs){
+        subunitig_ends.push_back(0); // Sentinel
+        std::reverse(subunitig_ends.begin(), subunitig_ends.end());
+    } else {
+        subunitig_ends = {0, nodes.size()}; // One big unitig
+    }
+    for(int64_t i = 1; i < subunitig_ends.size(); i++) {
+        int64_t len = subunitig_ends[i] - subunitig_ends[i-1];
+        unitigs_out << ">" << unitig_id << "\n";
+        unitigs_out.write(label.data() + subunitig_ends[i-1], len);
+        unitigs_out.write("\n", 1);
+    }
 
 }
 
@@ -66,7 +91,7 @@ void new_extract_unitigs(const DBG& dbg, const coloring_t& coloring, ostream& un
     write_log("Listing acyclic unitigs", LogLevel::MAJOR);
     for(DBG::Node v : dbg.all_nodes()){
         if(!is_first_kmer_of_unitig(dbg, v)) continue;
-        process_unitig_from(dbg, v, visited, unitigs_out, unitig_id++);
+        process_unitig_from(dbg, coloring, v, visited, unitigs_out, unitig_id++, split_by_colorset_runs);
     }
 
     // Only disjoint cyclic unitigs remain
@@ -74,7 +99,7 @@ void new_extract_unitigs(const DBG& dbg, const coloring_t& coloring, ostream& un
 
     for(DBG::Node v : dbg.all_nodes()) {
         if(visited[v.id]) continue;
-        process_unitig_from(dbg, v, visited, unitigs_out, unitig_id++);
+        process_unitig_from(dbg, coloring, v, visited, unitigs_out, unitig_id++, split_by_colorset_runs);
     }
 
     unitigs_out.flush();
