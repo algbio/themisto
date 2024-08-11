d9c1661a31c5a1f17ec142a34c5817dc5330474e
Jarno N. Alanko <alanko.jarno@gmail.com>
2024-08-11 17:00:50 +0300

    Make it compile

--- include/new_extract_unitigs.hh
+++ include/new_extract_unitigs.hh
@@ -46,7 +46,7 @@ void process_unitig_from(const DBG& dbg, const coloring_t& coloring, DBG::Node v
     std::tie(nodes, label) = walk_unitig_from(dbg, v);
     check_true(nodes.size() > 0, "BUG: empty unitig");
 
-    int64_t color_set_id = coloring.get_color_set_id(nodes.back());
+    int64_t color_set_id = coloring.get_color_set_id(nodes.back().id);
 
     for(int64_t pos = (int64_t)nodes.size()-1; pos >= 0; pos--){
         const DBG::Node& u = nodes[pos];
@@ -54,7 +54,7 @@ void process_unitig_from(const DBG& dbg, const coloring_t& coloring, DBG::Node v
         if(split_by_colorset_runs){
             // Color sets can change only at core k-mers. Otherwise the color set is the same as that
             // of the successor in the DBG.
-            int64_t new_color_set_id = coloring.is_core_kmer(u) ? coloring.get_color_set_id(u) : color_set_id;
+            int64_t new_color_set_id = coloring.is_core_kmer(u.id) ? coloring.get_color_set_id(u.id) : color_set_id;
             if(pos == (int64_t)nodes.size()-1 || new_color_set_id != color_set_id) {
                 subunitig_ends.push_back(pos+1); // Start a new subunitig
             }
