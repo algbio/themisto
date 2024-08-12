d448f7ab0bacadda6e8d186340e84b88c8824127
Jarno N. Alanko <alanko.jarno@gmail.com>
2024-08-12 11:11:50 +0300

    Move to .cpp file

--- include/new_extract_unitigs.hh
+++ include/new_extract_unitigs.hh
@@ -5,38 +5,10 @@
 #include "globals.hh"
 #include "coloring/Coloring.hh"
 
-bool is_first_kmer_of_unitig(const DBG& dbg, const DBG::Node& node) {
-    int64_t indeg = dbg.indegree(node);
-    if(indeg != 1){
-        return true;
-    } else {
-        DBG::Node u = dbg.pred(node);
-        return dbg.outdegree(u) > 1;
-    }
-}
+bool is_first_kmer_of_unitig(const DBG& dbg, const DBG::Node& node); 
 
 // Returns the sequence of nodes and the label of the unitig
-pair<vector<DBG::Node>, vector<char>> walk_unitig_from(const DBG& dbg, DBG::Node v) {
-    DBG::Node v0 = v;
-    vector<DBG::Node> nodes;
-    nodes.push_back(v);
-    
-    string first_kmer_label = dbg.get_node_label(v);
-    vector<char> label(first_kmer_label.begin(), first_kmer_label.end());
-
-    while(dbg.outdegree(v) == 1) {
-        DBG::Edge e = *(dbg.outedges(v).begin());
-
-        char c = e.label;
-        v = e.dest; // Follow edge
-        if(v != v0 && dbg.indegree(v) == 1) {
-            label.push_back(c);
-            nodes.push_back(v);
-        } else { break; }
-    }
-
-    return {nodes, label};
-}
+pair<vector<DBG::Node>, vector<char>> walk_unitig_from(const DBG& dbg, DBG::Node v);
 
 template<typename coloring_t>
 void process_unitig_from(const DBG& dbg, const coloring_t& coloring, DBG::Node v, vector<bool>& visited, ostream& unitigs_out, int64_t unitig_id, bool split_by_colorset_runs) {
---
+++ src/new_extract_unitigs.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "DBG.hh"
+#include "globals.hh"
+#include "coloring/Coloring.hh"
+
+bool is_first_kmer_of_unitig(const DBG& dbg, const DBG::Node& node) {
+    int64_t indeg = dbg.indegree(node);
+    if(indeg != 1){
+        return true;
+    } else {
+        DBG::Node u = dbg.pred(node);
+        return dbg.outdegree(u) > 1;
+    }
+}
+
+// Returns the sequence of nodes and the label of the unitig
+pair<vector<DBG::Node>, vector<char>> walk_unitig_from(const DBG& dbg, DBG::Node v) {
+    DBG::Node v0 = v;
+    vector<DBG::Node> nodes;
+    nodes.push_back(v);
+    
+    string first_kmer_label = dbg.get_node_label(v);
+    vector<char> label(first_kmer_label.begin(), first_kmer_label.end());
+
+    while(dbg.outdegree(v) == 1) {
+        DBG::Edge e = *(dbg.outedges(v).begin());
+
+        char c = e.label;
+        v = e.dest; // Follow edge
+        if(v != v0 && dbg.indegree(v) == 1) {
+            label.push_back(c);
+            nodes.push_back(v);
+        } else { break; }
+    }
+
+    return {nodes, label};
+}
